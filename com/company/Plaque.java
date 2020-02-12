62748b2ad441d9ea8511836821346f171be3c198
nathan vignal <37573225+nathan-vignal@users.noreply.github.com>
2020-02-12 23:05:05 +0100

    Add files via upload

---
+++ com/company/Plaque.java
@@ -0,0 +1,11 @@
+package com.company;
+import com.company.TypePiece;
+
+import java.util.ArrayList;
+
+public class Plaque {
+    private ArrayList<TypePiece> pieces;
+    short h;
+    short l;
+
+}
